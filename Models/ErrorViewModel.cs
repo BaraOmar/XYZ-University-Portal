8d6d50d0a032f45f25ebf68338367112b331ea5b
Bara <baraOmar@gmail.com>
2026-01-01 15:11:27 +0300

    Initial ASP.NET Core MVC project with Identity

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XYZUniversityPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
